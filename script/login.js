496e2ae244cf232c793d9f510c923e34697ca6ae
tushar <iktushar2020@gmail.com>
2025-02-25 13:24:30 +0600

    login page create fully done

---
+++ script/login.js
@@ -0,0 +1,24 @@
+document.getElementById("login_btn").addEventListener("click", function(event){
+    event.preventDefault();
+    const accountNumber = document.getElementById("account_number").value;
+    const accountPassword = document.getElementById("account_password").value;
+    const convertPin = parseInt(accountPassword);
+        if (
+            accountNumber.length === 11 &&
+            accountNumber.slice(0, 2) === "01" &&
+          !accountNumber.includes(" ") &&
+          !isNaN(accountNumber)
+        ) {
+          if (convertPin === 1234){
+            alert("Login Successful");
+          }
+          else{
+            alert("Login Failed");
+          }
+        }
+      else{
+        alert("Please Enter a Valid Number");
+      }
+    }
+     
+);
